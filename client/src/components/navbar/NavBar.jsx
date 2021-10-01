3758b7c259a703f68f700f4f2c292d8f9cee0d4f
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-10-01 16:08:24 -0400

    Routes modificated

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import logo from "../../assets/res/logo.png";
 import styles from "../../assets/styles/navbar.module.css";
@@ -6,10 +7,12 @@ import styles from "../../assets/styles/navbar.module.css";
 function NavBar() {
   return (
     <div className={styles.container}>
-      <div className="center">
-        <img src={logo} alt="soy_henry" />
-        <span className={styles.title}>SOY HENRY</span>
-      </div>
+      <Link to="/videogames">
+        <div className="center">
+          <img src={logo} alt="soy_henry" />
+          <span className={styles.title}>SOY HENRY</span>
+        </div>
+      </Link>
       <div className={styles.search}>
         <SearchBar />
       </div>
