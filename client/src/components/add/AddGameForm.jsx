26a749da39241c79d72c3ca37ddbe437c967b70c
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-10-04 17:50:15 -0400

    add videogame developed

--- client/src/components/add/AddGameForm.jsx
+++ client/src/components/add/AddGameForm.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 
-function AddGameForm() {
+function AddGameForm({ styles, videogame, handleOnChange, handleOnSubmit }) {
   return (
-    <div>
-      <h2>Add Videogame</h2>
+    <div className={`${styles.box} center`}>
+      <form>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={videogame.title}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            id="description"
+            value={videogame.description}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="released">Released</label>
+          <input
+            type="date"
+            name="released"
+            id="released"
+            value={videogame.released}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rating">Rating</label>
+          <input
+            type="number"
+            name="rating"
+            id="rating"
+            value={videogame.rating}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="platforms">Platforms</label>
+          <input
+            type="text"
+            name="platforms"
+            id="platforms"
+            value={videogame.platforms}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group text-center">
+          <button onClick={handleOnSubmit}>APPEND</button>
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/pages/AddGamesPage.jsx
+++ client/src/pages/AddGamesPage.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import AddGameForm from "../components/add/AddGameForm";
+import styles from "../assets/styles/add_box.module.css";
 
 function AddGamesPage() {
+  const [videogame, setVideogame] = React.useState({
+    title: "",
+    description: "",
+    released: "",
+    rating: "",
+    platforms: [],
+  });
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(videogame);
+  };
+
+  const handleOnChange = (e) => {
+    setVideogame({
+      ...videogame,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <>
-      <h2>Add Games Page</h2>
-    </>
+    <div className={`${styles.container} center`}>
+      <h2>ADD NEW VIDEOGAME</h2>
+      <AddGameForm
+        styles={styles}
+        videogame={videogame}
+        handleOnChange={handleOnChange}
+        handleOnSubmit={handleOnSubmit}
+      />
+    </div>
   );
 }
 
