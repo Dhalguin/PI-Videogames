168029b930f6985e08a5f98748a13ef0b423bac1
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-10-06 17:02:58 -0400

    include filters select

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -5,14 +5,30 @@ import logo from "../../assets/res/logo.png";
 import styles from "../../assets/styles/navbar.module.css";
 
 function NavBar() {
+  const [order, setOrder] = React.useState("append");
+
   return (
     <div className={styles.container}>
-      <Link to="/videogames" className="link">
-        <div className="center">
-          <img src={logo} alt="soy_henry" />
-          <span className={styles.title}>SOY HENRY</span>
-        </div>
-      </Link>
+      <div className={styles.otro}>
+        <Link to="/videogames" className="link">
+          <div className="center">
+            <img src={logo} alt="soy_henry" />
+            <span className={styles.title}>SOY HENRY</span>
+          </div>
+        </Link>
+      </div>
+
+      <div className={styles.filters}>
+        <label>ORDER BY</label>
+        <select defaultValue={order} onChange={(e) => setOrder(e.target.value)}>
+          <option value="append">append</option>
+          <option value="exists">exists</option>
+          <option value="genres">genres</option>
+          <option value="rating">rating</option>
+          <option value="asc">alphabet asc</option>
+          <option value="desc">Alphabet desc</option>
+        </select>
+      </div>
       <div className={styles.search}>
         <SearchBar />
       </div>
