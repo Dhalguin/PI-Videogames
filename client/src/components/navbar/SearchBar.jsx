efe5c726d37b9e925d6b6e8902e21a6aa2086c65
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-10-14 15:27:25 -0400

    redirect from search bar

--- client/src/components/navbar/SearchBar.jsx
+++ client/src/components/navbar/SearchBar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useLocation, useHistory } from "react-router-dom";
 import { getVideogames } from "../../redux/actions/gamesAction.js";
 import styles from "../../assets/styles/search_bar.module.css";
 
 function SearchBar() {
   const dispatch = useDispatch();
+  const location = useLocation().pathname;
+  const history = useHistory();
   const [title, setTitle] = React.useState("");
+  const baseRoute = "/videogames";
 
   const handleSubmit = () => {
+    if (location !== baseRoute) history.push(baseRoute);
     if (title) dispatch(getVideogames(1, 15, title));
     else dispatch(getVideogames(1, 20));
   };
